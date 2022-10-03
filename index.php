6b0d6dfe7a64e8d54b2b388a44063a21e881d905
miang <miguelamadormj@gmail.com>
2022-10-03 17:14:32 -0500

    add basic operations

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
 <?php
-if(isset($_GET['number_1']) && isset($_GET['number_2']) && is_numeric($_GET['number_1']) && is_numeric($_GET['number_2']))
+if(isset($_POST['number_1']) && isset($_POST['number_2']) && is_numeric($_POST['number_1']) && is_numeric($_POST['number_2']))
 {
-    
-$number_1 = $_GET['number_1']; 
-$number_2 = $_GET['number_2']; 
-$result = $number_1 + $number_2;
+
+$operation = $_POST['operation'];
+$number_1 = $_POST['number_1']; 
+$number_2 = $_POST['number_2'];
+
+switch ($operation) {
+    case '0':
+        $result = $number_1 + $number_2;        
+        break;
+    case '1':
+        $result = $number_1 - $number_2;        
+        break;
+    case '2':
+        $result = $number_1 * $number_2;        
+        break;
+    case '3':
+        if($number_1 == '0' || $number_2 == '0'){
+            $result =  'Error';
+        }
+        else {
+            $result = $number_1 / $number_2;     
+        }
+        break;
 }
-if(isset($_GET))[]
+}
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +36,7 @@ if(isset($_GET))[]
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
+
     <!-- Custom css-->
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="./css/style.css">
@@ -25,20 +45,26 @@ if(isset($_GET))[]
 <body>
     <article class="wrapper">
         <h2 class="title">PHP Calculator</h2>
-        <form action="index.php" method="get">
+
+        <form action="index.php" method="post">
             <input class="field" type="number" name="number_1">
+        
             <select class="field select"name="operation">
                 <option class="option" value="0">+</option>
                 <option class="option" value="1">-</option>
                 <option class="option" value="2">*</option>
                 <option class="option" value="3">/</option>
             </select>
+            
             <input class="field" type="number" name="number_2">
+            
             <button class="btn" type="submit">send</button>
         </form>
-        <?php if(isset($result)){?>
+        
+        <?php //if(isset($result)){?>
         <h3 class="result">Result: <?php echo $result?></h3>
-        <?php }?>
+        
+        <?php //} ?>
     </article>
 
 </body>
